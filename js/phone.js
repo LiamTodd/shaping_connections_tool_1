6a13ebc18a490b30b5cabf628c52a0831992cd5f
Liam <ltod0001@student.monash.edu>
2022-04-17 15:21:01 +1000

    adjust chart font on phone

--- js/phone.js
+++ js/phone.js
@@ -124,6 +124,18 @@ const normaliseToFives = (values) => {
     }
 }
 
+const capture = () => {
+    html2canvas(document.getElementById('to-capture')).then(function (canvas) {
+        const url = canvas.toDataURL("image/jpeg", 1)
+        debugBase64(url)
+    })
+}
+
+const debugBase64 = (base64URL) => {
+    const win = window.open();
+    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+}
+
 const initialSetUp = () => {
     console.log('here')
     document.getElementById('everything').innerHTML += 
@@ -179,11 +191,15 @@ const initialSetUp = () => {
     </div>
     <br><br>
 
-  <div>
-    <div style="width:${maxWidth}; padding: 10px">
+   <div>
+    <div style="width:${maxWidth}; padding: 10px" id='to-capture'>
       <h1 class="font1" style='font-size:25px'>Based on our assessment tool, your level of internet skills confidence looks like this:</h1>
       <div id='radar-container'></div>
     </div>
+    <div style='width:${maxWidth};text-align:center'>
+        <button onclick='capture()' class='btn see-result' >SAVE MY RESULT</button>
+    </div>
+
     `
 }
 
